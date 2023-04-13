a3c99c109d5779e83d2b724c678bfb141a1672af
DoanHongPhuc <doanhongphuc0111@gmail.com>
2023-04-13 13:36:02 +0700

    Updates

---
+++ precache-manifest.ec06f0320f598bab53455386f48d204a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2db8e38ca9933621ad8eefb9d76a61",
+    "url": "/Emoji_Search/index.html"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/Emoji_Search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "c5fa836a1202ef9b0768",
+    "url": "/Emoji_Search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/Emoji_Search/static/js/2.120a0f45.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/Emoji_Search/static/js/2.120a0f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fa836a1202ef9b0768",
+    "url": "/Emoji_Search/static/js/main.f7dd41d8.chunk.js"
+  },
+  {
+    "revision": "60770f0d5266efd236bd",
+    "url": "/Emoji_Search/static/js/runtime-main.d8056565.js"
+  }
+]);
